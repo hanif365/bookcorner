bcb44282bd458a9c147d33f9b91b1428c1d95903
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-05-29 02:42:45 +0600

    create LogIn Component

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LogIn.css';
+
+const LogIn = () => {
+    return (
+        <div>
+            <h1>This is Login Page</h1>
+        </div>
+    );
+};
+
+export default LogIn;
