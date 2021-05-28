ab15125d23b4b216369139a52953307c4fc74ec1
hanif365 <m.a.hanifkhan64@gmail.com>
2021-05-29 03:01:06 +0600

    Route declared

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { createContext, useState } from 'react';
 import './App.css';
 import Home from './components/Home/Home/Home';
-import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import About from './components/About/About';
+import Books from './components/Books/Books';
+import Contact from './components/Contact/Contact';
 
 export const UserContext = createContext();
 
@@ -18,6 +20,21 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/books">
+            <Books></Books>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/login">
 
           </Route>
         </Switch>
