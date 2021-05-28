cd061027576fa622cae0f00a7130631771e2dd2b
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-05-28 23:46:42 +0600

    SignUp Page Added from developer-2

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,6 +4,8 @@ const SignUp = () => {
     return (
         <div>
             <h1>This is SignUp Page</h1>
+            <h1>This is SignUp Page1</h1>
+            <h1>This is SignUp Page2</h1>
         </div>
     );
 };
