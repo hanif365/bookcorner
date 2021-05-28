c8a769ae0741cf1245990c2dc31d57ca4d6af331
hanif365 <m.a.hanifkhan64@gmail.com>
2021-05-29 02:32:42 +0600

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import Home from './components/Home/Home/Home';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-      <Home></Home>
-      <h1>Developer 1 test3</h1>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../Shared/Navbar/Navbar';
+import HeaderContent from '../HeaderContent/HeaderContent';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <HeaderContent></HeaderContent>
         </div>
     );
 };
