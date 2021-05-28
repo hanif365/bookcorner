a12399cc8cf496aa8649fb66e09e74e0807fd9fe
hanif365 <m.a.hanifkhan64@gmail.com>
2021-05-28 23:02:35 +0600

    Navbar added

---
+++ src/components/Shared/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#">Book Corner</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ms-auto">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#">Disabled</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
