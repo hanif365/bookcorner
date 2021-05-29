cbab0106d531d2da19fd961db595855c56466c3b
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-05-30 03:31:18 +0600

    updated from dev-2

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -29,12 +29,27 @@ const LogIn = () => {
         <div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
-                        <button onClick={handleGoogleSignIn} className="google-signIn-btn"><img className="google-logo" src={google} alt="google-logo" /><span>Sign In With Google</span></button>
+                    <div className="form-input">
+                        <div class="mb-3">
+                            <label for="formGroupExampleInput" class="form-label"><b>User Name</b></label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="User Name" />
+                        </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label"><b>Email address</b></label>/
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label"><b>Password</b></label>
+                                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password" />
+                            </div>
+                            <button type="submit" className="form-btn btn-style">SUBMIT</button>
+                        </div>
+                        <div className="col-md-12">
+                            <button onClick={handleGoogleSignIn} className="google-signIn-btn"><img className="google-logo" src={google} alt="google-logo" /><span>Sign In With Google</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
