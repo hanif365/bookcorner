8758dc139a5cfe9211e7eb28a14f398081448258
hanif365 <m.a.hanifkhan64@gmail.com>
2021-05-30 12:16:02 +0600

    Bootom to top icon added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
         <div className="bg-info py-3  d-flex align-items-center justify-content-center">
             <h6>@2021 <span className="text-colorful">Book Corner</span> All Rights Reserved</h6>
+            <div>
+                <a href="#" className="bottom-to-top"><FontAwesomeIcon icon={faArrowUp} size="2x" /></a>
+            </div>
         </div>
     );
 };
